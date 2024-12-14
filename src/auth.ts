3a21a8bf2d20527e523a0f17c3c373ff9d0e2941
Tag6362 <tag6362@gmail.com>
2024-12-14 17:57:59 +0100

    fix auth prisma import

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,7 @@
 import NextAuth, { AuthOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import { prisma } from "@/prisma";
+import { prisma } from "./lib/prisma";
 
 export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
