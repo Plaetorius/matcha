54deffa63307bb8198c843065d6e2e16a0911f24
Tag6362 <tag6362@gmail.com>
2024-12-24 15:44:23 +0100

    Loading and static chat page

---
+++ src/app/members/[userId]/chat/page.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import PageClient from './pageClient'
+
+export default function ChatPage() {
+	return <PageClient />;
+}
---
+++ src/app/members/[userId]/chat/pageClient.tsx
@@ -0,0 +1,17 @@
+import {
+	CardBody,
+	CardHeader,
+	Divider
+} from "@nextui-org/react";
+
+export default function PageClient() {
+	return (
+		<>
+			<CardHeader className="text-2xl font-semibold text-default">
+				Chat
+			</CardHeader>
+			<Divider />
+			<CardBody>Chat goes here</CardBody>
+		</>
+	)
+}
